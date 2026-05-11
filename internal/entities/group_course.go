d494ea2c2f67a31d3d253c564c6c1c89f91f4824
TwiLightDM <dmitriy011004@gmail.com>
2026-05-11 19:45:35 +0400

    Убраны лишнее поле и лишняя ошибка

--- internal/entities/group_course.go
+++ internal/entities/group_course.go
@@ -1,10 +1,7 @@
 package entities
 
-import "gorm.io/gorm"
-
 type GroupCourse struct {
-	Id        string
-	CourseId  string
-	GroupId   string
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Id       string
+	CourseId string
+	GroupId  string
 }
--- internal/errs/errors.go
+++ internal/errs/errors.go
@@ -3,7 +3,6 @@ package errs
 import "errors"
 
 var (
-	ErrRecordNotFound              = errors.New("record not found")
-	ErrDuplicateKey                = errors.New("duplicate key")
-	ErrModuleIdAndCourseIdNotFound = errors.New("module id and course id not found")
+	ErrRecordNotFound = errors.New("record not found")
+	ErrDuplicateKey   = errors.New("duplicate key")
 )
